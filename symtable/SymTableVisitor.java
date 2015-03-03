9a7f1ef51aed597139ed108f3acc94c38380e166
aristeia <jsims@pugetsound.edu>
2015-03-02 17:06:58 -0800

    not on drugs

--- symtable/SymTableVisitor.java
+++ symtable/SymTableVisitor.java
@@ -66,7 +66,7 @@ public class SymTableVisitor extends DepthFirstAdapter
         try {
             table.putMain(node.getId().toString(), node.getId().toString());}
         catch(Exception e){
-            throw e;
+            //throw e;
         }
         
         node.getId().apply(this);
