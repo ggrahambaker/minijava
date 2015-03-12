a5596086c0f897187d28b2d924bb42bac0dc9a59
aristeia <jsims@pugetsound.edu>
2015-03-11 23:21:21 -0700

    stuff

--- symtable/TypeCheckVisitor.java
+++ symtable/TypeCheckVisitor.java
@@ -579,18 +579,25 @@ public class TypeCheckVisitor extends DepthFirstAdapter
     public void caseALtExp(ALtExp node)
     {
         inALtExp(node);
-        if(node.getLeft() != null)
-	    if(node.getLeft() instanceof ATrueExp || node.getLeft() instanceof AFalseExp)	    
-		node.getLeft().apply(this);
-	    else {
+	Node left = null;
+	Node right = null;
+        if(node.getLeft() != null){
+	    node.getLeft().apply(this);
+	    left = medium;
+	    if(!(left instanceof ATrueExp || left instanceof AFalseExp)){
 		System.out.println("Error: "+node.getLeft().toString()+" is not of type boolean");
-		System.exit(1);}	
-	if(node.getRight() != null)
-	    if(node.getRight() instanceof ATrueExp || node.getLeft() instanceof AFalseExp)
-		node.getRight().apply(this);
-	    else {
-		System.out.println("Error: "+node.getRight().toString()+" is not of type boolean");
-		System.exit(1);	    }	
+		System.exit(1);}}
+	if(node.getRight() != null){
+	    node.getRight().apply(this);
+	    right = medium;
+	    if(!(right instanceof ATrueExp || right instanceof AFalseExp)){
+          	System.out.println("Error: "+node.getRight().toString()+" is not of type boolean");
+		System.exit(1);
+	    }
+	}
+	boolean result = Integer.parseInt(((ANumExp)left).getNum().getText()) +Integer.parseInt(((ANumExp)left).getNum().getText());
+	TNum con = new TNum(Integer.toString(result));
+	medium = new ANumExp(con);
         outALtExp(node);
     }
 
@@ -608,7 +615,6 @@ public class TypeCheckVisitor extends DepthFirstAdapter
     public void caseAPlusExp(APlusExp node)
     {
         inAPlusExp(node);
-<<<<<<< HEAD
 	Node left = null;
 	Node right = null;
         if(node.getLeft() != null){
@@ -625,39 +631,10 @@ public class TypeCheckVisitor extends DepthFirstAdapter
 		System.exit(1);
 	    }
 	}
-	int result = Integer.parseInt(((ANumExp)medium).getNum().getText()) +Integer.parseInt(((ANumExp)left).getNum().getText());
-	//System.out.println(result + " -> the result!"); 
+	int result = Integer.parseInt(((ANumExp)left).getNum().getText()) +Integer.parseInt(((ANumExp)right).getNum().getText());
 	TNum con = new TNum(Integer.toString(result));
 	medium = new ANumExp(con);
-	//System.out.println(medium.toString());
-	//System.out.println(medium.getClass());
 	outAPlusExp(node);
-=======
-        
-        Node left = null;
-        if(node.getLeft() != null)
-	       if(node.getLeft() instanceof ANumExp){
-          node.getLeft().apply(this);
-          left = medium;
-         }
-	       else {
-		      System.out.println("Error: "+node.getLeft().toString()+" is not of type int");
-		      System.exit(1);}		
-        if(node.getRight() != null)
-          if(node.getRight() instanceof ANumExp){
-            node.getRight().apply(this);
-            int result = Integer.parseInt(((ANumExp)medium).getNum().getText()) +Integer.parseInt(((ANumExp)left).getNum().getText());
-            System.out.println(result + " -> the result!"); 
-            TNum con = new TNum(Integer.toString(result));
-            medium = new ANumExp(con);
-            System.out.println(medium.toString());
-            System.out.println(medium.getClass());
-          } else {
-          	System.out.println("Error: "+node.getRight().toString()+" is not of type int");
-            System.exit(1);
-          }
-        outAPlusExp(node);
->>>>>>> 6415276ccd876d6e17026d3a494593b7557cd781
     }
 
     public void inAMinusExp(AMinusExp node)
