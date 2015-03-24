641511643d4d3867be07081b8ee1d257c71b02e0
aristeia <jsims@pugetsound.edu>
2015-03-23 22:22:59 -0700

    modified varinfo

--- symtable/VarInfo.java
+++ symtable/VarInfo.java
@@ -1,7 +1,8 @@
 package symtable;
 
 import minijava.node.PType;
-//import Arch.Access;
+import Arch.Access;
+
 
 /** 
  * VarInfo records information about a single local variable.  We need
@@ -10,17 +11,17 @@ import minijava.node.PType;
  * will once we get to the IRT checkpoint...
  */
 public class VarInfo {
-   private PType type;
+    private PType type;
+    //  private InFrame acc   ;
    
    public VarInfo(PType t) { type = t; }
    public PType getType() { return type; }
    public String toString() { return Types.toStr(type); }
-  
    /* 
     * Stuff to add once we got the the IRT phase
-
+    */
    private Access acc;
-   public Access getAccess() { return acc; }
+   public Access getAccess() { return access; }
    public void setAccess(Access a) { acc = a; }
-   */  
+      
 }
